2fad6e712def36632a50e1053b23340286ea9397
Antoni Szymański <antoni.szymanski+github@proton.me>
2026-02-21 06:37:41 +0100

    refactor(inv2go): remove unsafe usage, reorder code

--- inv2go/inv2go.go
+++ inv2go/inv2go.go
@@ -4,14 +4,77 @@
 package main
 
 import (
+	"fmt"
 	"go/format"
 	"io"
 	"os"
 	"regexp"
 	"strings"
 	"unicode"
-	"unsafe"
 )
+
+func main() {
+	output, err := run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	if output != "" {
+		fmt.Fprintln(os.Stdout, output) //nolint:errcheck
+	}
+}
+
+func run() (string, error) {
+	input, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return "", err
+	}
+
+	processed := string(input)
+	processed = r.Replace(processed)
+	processed = re1.ReplaceAllString(processed, `: []$1`)
+	processed = re2.ReplaceAllString(processed, `: []$1`)
+	processed = re3.ReplaceAllString(processed, `[]$1`)
+	processed = re4.ReplaceAllStringFunc(processed, func(s string) string {
+		switch s[0] {
+		case '[':
+			return "[]"
+		case ']':
+			return ""
+		default:
+			panic("unreachable")
+		}
+	})
+	processed = re5.ReplaceAllString(processed, `int$1`)
+	processed = re6.ReplaceAllString(processed, `float$1`)
+	processed = strings.ReplaceAll(processed, `: Number // Integer`, ": Number")
+	processed = strings.ReplaceAll(processed, "Number", "int64")
+	processed = strings.ReplaceAll(processed, ": int64 // Unix timestamp", ": time.Time")
+	processed = re7.ReplaceAllString(processed, `: option.Option[$1]`)
+	processed = re8.ReplaceAllString(processed, `: string // "$1"`)
+	processed = re9.ReplaceAllString(processed, `: int64`)
+	processed = re10.ReplaceAllString(processed, `: bool`)
+	processed = re11.ReplaceAllLiteralString(processed, ": []any")
+	processed = strings.ReplaceAll(processed, ": null", ": any")
+	processed = re12.ReplaceAllStringFunc(processed, func(s string) string {
+		runes := []rune(s)
+		runes = runes[1 : len(runes)-2]
+		runes[0] = unicode.ToUpper(runes[0])
+		return string(runes)
+	})
+	processed = re13.ReplaceAllStringFunc(processed, func(s string) string {
+		runes := []rune(s)
+		runes = runes[1 : len(runes)-3]
+		runes[0] = unicode.ToUpper(runes[0])
+		return string(runes)
+	})
+	processed = re14.ReplaceAllLiteralString(processed, "\n")
+
+	formatted, err := format.Source([]byte(processed))
+	if err != nil {
+		return processed, err
+	}
+	return string(formatted), nil
+}
 
 var (
 	r = strings.NewReplacer(
@@ -27,76 +90,12 @@ var (
 	re4  = regexp.MustCompile(`\[\s*|]`)                         // array types p4
 	re5  = regexp.MustCompile(`Int(\d+)`)                        // int types
 	re6  = regexp.MustCompile(`Float(\d+)`)                      // float types
-	re7  = regexp.MustCompile(`: Number // Integer`)             // Number type
-	re8  = regexp.MustCompile(`: (\w+)\?`)                       // optional types
-	re9  = regexp.MustCompile(`: "(.+)"(?: // Constant)?`)       // string constants
-	re10 = regexp.MustCompile(`: -?\d+`)                         // numeric literal values
-	re11 = regexp.MustCompile(`: (?:true|false)`)                // boolean literal values
-	re12 = regexp.MustCompile(`(?m): \[]\s*(?://.*)?$`)          // empty arrays
-	re13 = regexp.MustCompile(`".+":`)                           // field names p1
-	re14 = regexp.MustCompile(`".+:"`)                           // field names p2 (typo)
-	re15 = regexp.MustCompile(`\n?\s*\n`)                        // excessive newlines
+	re7  = regexp.MustCompile(`: (\w+)\?`)                       // optional types
+	re8  = regexp.MustCompile(`: "(.+)"(?: // Constant)?`)       // string constants
+	re9  = regexp.MustCompile(`: -?\d+`)                         // numeric literal values
+	re10 = regexp.MustCompile(`: (?:true|false)`)                // boolean literal values
+	re11 = regexp.MustCompile(`(?m): \[]\s*(?://.*)?$`)          // empty arrays
+	re12 = regexp.MustCompile(`".+":`)                           // field names p1
+	re13 = regexp.MustCompile(`".+:"`)                           // field names p2 (typo)
+	re14 = regexp.MustCompile(`\n?\s*\n`)                        // excessive newlines
 )
-
-func main() {
-	data, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		panic(err)
-	}
-	s := bytes2string(data)
-
-	s = r.Replace(s)
-	s = re1.ReplaceAllString(s, `: []$1`)
-	s = re2.ReplaceAllString(s, `: []$1`)
-	s = re3.ReplaceAllString(s, `[]$1`)
-	s = re4.ReplaceAllStringFunc(s, func(s string) string {
-		switch s[0] {
-		case '[':
-			return "[]"
-		case ']':
-			return ""
-		default:
-			panic("unreachable")
-		}
-	})
-	s = re5.ReplaceAllString(s, `int$1`)
-	s = re6.ReplaceAllString(s, `float$1`)
-	s = re7.ReplaceAllLiteralString(s, ": Number")
-	s = strings.ReplaceAll(s, "Number", "int64")
-	s = strings.ReplaceAll(s, ": int64 // Unix timestamp", ": time.Time")
-	s = re8.ReplaceAllString(s, `: option.Option[$1]`)
-	s = re9.ReplaceAllString(s, `: string // "$1"`)
-	s = re10.ReplaceAllString(s, `: int64`)
-	s = re11.ReplaceAllString(s, `: bool`)
-	s = re12.ReplaceAllLiteralString(s, ": []any")
-	s = strings.ReplaceAll(s, ": null", ": any")
-	s = re13.ReplaceAllStringFunc(s, func(s string) string {
-		runes := []rune(s)
-		runes = runes[1 : len(runes)-2]
-		runes[0] = unicode.ToUpper(runes[0])
-		return string(runes)
-	})
-	s = re14.ReplaceAllStringFunc(s, func(s string) string {
-		runes := []rune(s)
-		runes = runes[1 : len(runes)-3]
-		runes[0] = unicode.ToUpper(runes[0])
-		return string(runes)
-	})
-	s = re15.ReplaceAllLiteralString(s, "\n")
-
-	data, err = format.Source(string2bytes(s))
-	if err != nil {
-		os.Stderr.WriteString(err.Error()) //nolint:errcheck
-		os.Stderr.WriteString("\n")        //nolint:errcheck
-		data = string2bytes(s)
-	}
-	os.Stdout.Write(data) //nolint:errcheck
-}
-
-func string2bytes(s string) []byte {
-	return unsafe.Slice(unsafe.StringData(s), len(s))
-}
-
-func bytes2string(b []byte) string {
-	return unsafe.String(unsafe.SliceData(b), len(b))
-}
